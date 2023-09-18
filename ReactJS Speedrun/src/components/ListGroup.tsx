05c5ecea42952d444fc2e5c2e155a78780dea9a3
Rajeev Tapadia <rtapadia730@gmail.com>
2023-09-19 01:29:02 +0530

    useState

--- ReactJS Speedrun/src/components/ListGroup.tsx
+++ ReactJS Speedrun/src/components/ListGroup.tsx
@@ -1,23 +1,30 @@
 // return can only contain one element
 // to add multiple elements we use fragment <> </>
 
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 
 const ListGroup = () => {
   let items = ["pune", "nagpur", "jalna", "bhosari"];
   // items = [];
 
-  // event handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  // hooks
+  const [selected, setSelected] = useState(-1)
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-            // passing the ref not actually calling the function 
-          <li className="list-group-item" onClick={handleClick} key={item}>
+        {items.map((item, index) => (
+          // passing the ref not actually calling the function
+          <li
+            className={
+              index === selected ? "list-group-item active" : " list-group-item"
+            }
+            // arrow function is passed in onClick
+            onClick={() => {setSelected(index)}}
+            key={item}
+          >
             {item}
           </li>
         ))}
