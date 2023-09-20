6b89c24f1f3a70e48ade20d39d0bf5fc36212b37
Rajeev Tapadia <rtapadia730@gmail.com>
2023-09-20 23:50:35 +0530

    adjkfgui

--- ReactJS Speedrun/src/App.tsx
+++ ReactJS Speedrun/src/App.tsx
@@ -11,13 +11,13 @@ function App() {
 
   return (
     <div>
-      {/* <ListGroup items={items} heading='cities' onSelection={handleSelectedItem}/>
-      <ListGroup heading='second list' items={['i', 'am', 'god']} onSelection={doNothing}/> */}
       <Alert>
         {/* this content is recieved to parent as prop named children */}
         <h1>Hello world</h1>
         <div>lolfuck</div>
       </Alert>
+      <ListGroup items={items} heading='cities' onSelection={handleSelectedItem}/>
+      <ListGroup heading='second list' items={['i', 'am', 'god']} onSelection={doNothing}/>
     </div>
   );
 }
