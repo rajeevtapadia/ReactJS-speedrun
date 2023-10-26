f0886846592606c5be321d4d9d80aa59f9bd7224
Rajeev Tapadia <rtapadia730@gmail.com>
2023-10-26 16:41:21 +0530

    quiz list on admin dash

--- quiz-app/src/Components/AdminDash.js
+++ quiz-app/src/Components/AdminDash.js
@@ -1,45 +1,51 @@
-import React from "react";
+import {useEffect, useState} from "react";
 import { Link } from "react-router-dom";
-import { db } from "../firebase"
-import { getdoc, doc } from "firebase/firestore"
+import { db } from "../firebase";
+import { getDocs, collection } from "firebase/firestore";
 
 const AdminDash = ({ admin }) => {
-  const quizes = [
-    { no: 1, name: "quiz1", attempts: 2, code: "qoieut" },
-    { no: 2, name: "quiz1", attempts: 2, code: "qoieut" },
-    { no: 3, name: "quiz1", attempts: 2, code: "qoieut" },
-  ];
-
-  const fetchQuizes = () => {
-//    const docRef = doc(db, "Admins/ ")
-  }
-
-  const table = quizes.map((quiz) => (
-    <tr key={quiz.no}>
-      <td>{quiz.no}</td>
-      <td>{quiz.name}</td>
-      <td>{quiz.attempts}</td>
-      <td>{quiz.code}</td>
-    </tr>
-  ));
+  const [quizTable, setQuizTable] = useState(null)
+  useEffect(() => {
+    const fetchQuizes = async () => {
+      const quizes = [];
+      const docs = await getDocs(collection(db, `Admins/${admin}/Quizes`));
+      let index = 1;
+      docs.forEach((doc) => {
+        quizes.push({ id: index++, name: doc.data().quizName });
+      });
+      return quizes;
+    };
+    fetchQuizes().then((quizes) => {
+      const table = quizes.map((quiz) => (
+        <tr key={quiz.id}>
+          <td>{quiz.id}</td>
+          <td>{quiz.name}</td>
+          {/*<td>{quiz.attempts}</td>*/}
+          {/*<td>{quiz.code}</td>*/}
+        </tr>
+      ));
+    console.log(table);
+    setQuizTable(table);
+    });
+  }, []);
 
   return (
     <>
-      <div className='flex justify-center'>Admin Dashboard</div>
-    <div className='flex flex-col h-screen justify-center items-center'>
-      <button className="btn btn-primary">
-        <Link to={"/admin/create-quiz"}>Create Quiz</Link>
-      </button>
-      <table>
-        <tr>
-          <th>Sr No</th>
-          <th> Quiz Name</th>
-          <th>No of Attempts</th>
-          <th>Quiz Code</th>
-        </tr>
-        {table}
-      </table>
-    </div>
+      <div className="flex justify-center">Admin Dashboard</div>
+      <div className="flex flex-col h-screen justify-center items-center">
+        <button className="btn btn-primary">
+          <Link to={"/admin/create-quiz"}>Create Quiz</Link>
+        </button>
+        <table>
+          <tr>
+            <th>Sr No</th>
+            <th>Quiz Name</th>
+            <th>No of Attempts</th>
+            <th>Quiz Code</th>
+          </tr>
+          {quizTable}
+        </table>
+      </div>
     </>
   );
 };
